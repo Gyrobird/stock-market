d7949eb6ef868513dcaf259dd22a37ff154dda2e
Animesh <en19el301022@medicaps.ac.in>
2023-03-24 17:02:18 +0530

    new addition of codes and axis 1st graph

--- axisbanktry.py
+++ axisbanktry.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#%matplotlib inline
+
 from matplotlib.pylab import rcParams
 import yfinance as yf
 from keras.models import Sequential
@@ -9,19 +9,25 @@ from keras.layers import LSTM,Dropout,Dense
 from sklearn.preprocessing import MinMaxScaler
 rcParams['figure.figsize']=20,10
 df = yf.download("AAPL", start="2021-01-01", end="2021-12-31")
+
+df["Date"] = df.index # create a new column "date" that contains the index values (which are dates)
 df["Date"]=pd.to_datetime(df.Date,format="%Y-%m-%d")
 df.index=df['Date']
 plt.figure(figsize=(16,8))
 plt.plot(df["Close"],label='Close Price history')
 plt.show()
+
+#traing the data
+
 data=df.sort_index(ascending=True,axis=0)
 new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
 for i in range(0,len(data)):
     new_dataset["Date"][i]=data['Date'][i]
     new_dataset["Close"][i]=data["Close"][i]
-    scaler=MinMaxScaler(feature_range=(0,1))
-final_dataset=new_dataset.values
-
+scaler=MinMaxScaler(feature_range=(0,1))
+final_dataset=scaler.fit_transform(new_dataset.values)
+final_dataset = new_dataset.drop("Date", axis=1).values
+final_dataset = scaler.fit_transform(final_dataset)
 train_data=final_dataset[0:5000,:]
 valid_data=final_dataset[5000:,:]
 
@@ -44,6 +50,7 @@ inputs_data=scaler.transform(inputs_data)
 lstm_model.compile(loss='mean_squared_error',optimizer='adam')
 lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2) # type: ignore
 X_test=[]
+
 for i in range(60,inputs_data.shape[0]):
     X_test.append(inputs_data[i-60:i,0])
 X_test=np.array(X_test)
@@ -54,5 +61,30 @@ lstm_model.save("saved_model.h5")
 train_data=new_dataset[:5000]
 valid_data=new_dataset[5000:]
 valid_data['Predictions']=predicted_closing_price
-plt.plot(train_data["Close"])
-plt.plot(valid_data[['Close',"Predictions"]])
+#plt.plot(train_data["Close"])
+#plt.plot(valid_data[['Close',"Predictions"]])
+
+
+#new adiitionss
+lstm_model = load_model("saved_model.h5")
+
+# prepare test data for prediction
+inputs_data = new_dataset[len(new_dataset)-len(valid_data)-60:].values
+inputs_data = inputs_data.reshape(-1,1)
+inputs_data = scaler.transform(inputs_data)
+
+# prepare test data with sliding window
+X_test = []
+for i in range(60, inputs_data.shape[0]):
+    X_test.append(inputs_data[i-60:i,0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+# make predictions
+predicted_closing_price = lstm_model.predict(X_test)
+predicted_closing_price = scaler.inverse_transform(predicted_closing_price)
+
+# plot the predictions
+plt.plot(valid_data["Close"])
+plt.plot(predicted_closing_price)
+plt.show()
